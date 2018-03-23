8d92db2d29572767caacca000d690b787a86edf2
LiliyaShalamova <shalamova-lilia@rambler.ru>
2018-03-23 13:13:25 +0500

    Начало

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+var menu = "<ul class=\"lcol_menu\">\n" +
+    "\t<li class=\"homepage\"><a href=\"index.html\">Главная страница</a></li>\n" +
+    "\t<li class=\"category\"><a href=\"first_semester.html\">1 семестр</a></li>\n" +
+    "\t<li><a href=\"osnovny_ponyatiya.html\">Основные понятия</a></li>\n" +
+    "\t<li><a href=\"uravneniya_1_poryadka.html\">Уравнения 1-го порядка</a></li>\n" +
+    "\t<li><a href=\"du_vyshih_porydkov_i_systemu.html\">ДУ высших порядков и системы</a></li>\n" +
+    "\t<li><a href=\"lineynue_du_n_poryadka.html\">Линейные ДУ n-го порядка</a></li>\n" +
+    "\t<li class=\"category\"><a href=\"/second_semestr.html\">2 семестр</a></li>\n" +
+    "\t<li><a href=\"lineynue_du_n_poryadka_s_post_koff.html\">Линейные ДУ n-го порядка с постоянными коэффицентами</a></li>\n" +
+    "\t<li><a href=\"systems_lineynuh_du.html\">Системы линейных ДУ</a></li>\n" +
+    "\t<li><a href=\"interpretation.html\">Механическая интерпретация ДУ</a></li>\n" +
+    "\t<li><a href=\"stability.html\">Устойчивость</a></li>\n" +
+    "\t<li><a href=\"differential_equations.html\">Разностные уравнения</a></li>\n" +
+    "\t<li class=\"category\"><a href=\"#\">Методы вычислений</a></li>\n" +
+    "\t<li><a href=\"numerical_solution_nonlinear_equations.html\">Численное решение нелинейных уравнений</a></li>\n" +
+	"\t<li><a href=\"numerical_solution_linear_systems.html\">Численные методы решения линейных систем</a></li>\n" +
+    "\t<li class=\"category\"><a href=\"#\">Прочее</a></li>\n" +
+    "\t<li><a href=\"literature.html\">Литература</a></li>\n" +
+    "\t<li><a href=\"formuls.html\">Формулы</a></li>\n" +
+    "</ul>";
+
+$("#left_col").html(menu);
